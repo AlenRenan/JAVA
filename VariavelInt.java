bf5a8fc6b2e40790e369192e4a469162b168d205
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-02-20 10:40:55 -0300

    Add files via upload

---
+++ VariavelInt.java
@@ -0,0 +1,12 @@
+class VariavelInt{
+	public static void main (String arg[]){
+			// declaração variavel inteira
+			int x = 10; // atribuição de valor
+			int y = 2;
+			// imprime no terminal
+			System.out.print ("Soma\t" + (x+y) );
+			System.out.print ("\nDivisao\t" + (x/y) );
+			System.out.print ("\nMultiplicacao\t" + (x*y) );
+			System.out.print ("\nSubtracao\t" + (x-y) );
+	}
+}
