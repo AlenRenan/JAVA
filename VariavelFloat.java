c43b2352a6a25d97cfc35f823dabcd440f3059f7
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-02-20 10:32:10 -0300

    Add files via upload

---
+++ VariavelFloat.java
@@ -0,0 +1,12 @@
+class VariavelFloat{
+	public static void main (String arg[]){
+			// declaração variavel inteira
+			float x = 10; // atribuição de valor de "x"
+			float y = 22; // atribuição de valor de "y"
+			// imprime no terminal
+			System.out.print ("Soma\t" + (x+y) );
+			System.out.print ("\nDivisao\t" + (y/x) );
+			System.out.print ("\nMultiplicacao\t" + (x*y) );
+			System.out.print ("\nSubtracao\t" + (y-x) );
+	}
+}
