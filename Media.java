26b7e5eec7f35fe3f8e53aa24fd0f8288590b53f
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-03-04 12:04:42 -0300

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,10 @@
+
+public class Media{
+    
+    void Valores(double valor1, double valor2){
+        
+        double  media = (valor1 + valor2)/2;
+       
+    System.out.printf("A media entre %.1f e %.1f = %f: ", valor1 , valor2 , media);
+    }
+}
