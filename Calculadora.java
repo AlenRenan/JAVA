202569be671ec42425fef0fe994fad0c14eb2703
AlenRenan <53836875+AlenRenan@users.noreply.github.com>
2020-03-04 12:03:49 -0300

    Add files via upload

--- Calculadora.java
+++ Calculadora.java
@@ -1,25 +1,24 @@
-import javax.swing.JOptionPane;
-public class Calculadora{
-    public static void main (String arg[]){
-        //Entrada via teclado
-        String s1 =  JOptionPane.showInputDialog("Forneca o valor 1: ");
-        String s2 = JOptionPane.showInputDialog("Forneca o valor 2: ");
 
-        // Converte para valor numérico
-        int valor1 = Integer.parseInt(s1);
-        int valor2 = Integer.parseInt(s2);
-        //Declara a variável
-        int som = valor1 + valor2;
-        int div = valor1 / valor2;
-        int mult = valor1 * valor2;
-        int sub = valor1 - valor2;
-       
-        System.out.println("A soma dos numeros da: " + som);
-        System.out.println("A divisao dos numeros da: " + div);
-        System.out.println("A multiplicacao dos numeros da: " + mult);
-        System.out.println("A subtracao dos numeros da: " + sub);
-
-         
-
+public class Calculadora {
+    void somar(float valor1 , float valor2){
+        System.out.println ("Soma =" + (valor1 + valor2 ));    
+    }         
+      void subtrair(float valor1 , float valor2){
+        System.out.println ("Subtracao = "  + (valor1 - valor2 ));
     }
+    
+      void multiplicar(float valor1 , float valor2){
+        System.out.println ("Multiplicacao = "  + (valor1 * valor2 ));
+    }
+    
+      void dividir(float valor1 , float valor2){
+        System.out.println ("Divisao = "  + (valor1 / valor2 ));
 }
+       void CalcularSomaMultiplicacao(float valor1 , float valor2){
+           somar (valor1, valor2);
+           multiplicar (valor1, valor2);
+
+}
+}
+
+
